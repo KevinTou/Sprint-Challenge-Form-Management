9360a9fadd0ca6f5b4be5f887362ff228451dfd6
Kevin Tou <toukevin@gmail.com>
2019-08-09 11:55:48 -0700

    Updated tests

--- client/src/components/Forms/UserForm.test.js
+++ client/src/components/Forms/UserForm.test.js
@@ -5,7 +5,17 @@ import UserForm from './UserForm';
 
 describe('<UserForm />', () => {
   it('submits the form', async () => {
+    // const userForm = render(<UserForm type={'Register'} />);
+    // console.log(userForm.debug());
     const { getByText, findByTestId } = render(<UserForm type={'Register'} />);
+
+    const username = await findByTestId('username');
+    const password = await findByTestId('password');
+
     fireEvent.click(getByText('Register'));
+
+    // Successful submission resets back to ''
+    expect(username.value).toBe('');
+    expect(password.value).toBe('');
   });
 });
